7bccfec66db3ee47c29af869a2f7d12141ea936f
RockySeven3161 <babaei.alireza580@GMAIL.COM>
2016-12-09 00:58:37 +0330

    Update linkpv.lua

--- plugins/linkpv.lua
+++ plugins/linkpv.lua
@@ -2,21 +2,22 @@ do
 
 function run(msg, matches)
        if not is_momod(msg) then
-        return "مجاز برای مدیر و ادمین ها"
+        return "For Higher Ranks"
        end
     local data = load_data(_config.moderation.data)
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
        if not group_link then 
-        return "اول باید لینک جدید ایجاد کنید"
+        return "First type /newlink !"
        end
-         local text = "لینک گروه:\n"..group_link
+         local text = "Group Link :\n"..group_link
           send_large_msg('user#id'..msg.from.id, text, ok_cb, false)
-           return "لینک به pv ارسال شد"
+           return "Link Has Been Sent To Your Pv."
 end
 
 return {
   patterns = {
     "^[/#!]([Ll]inkpv)$"
+    "^([Ll]inkpv)$"
   },
   run = run
 }
